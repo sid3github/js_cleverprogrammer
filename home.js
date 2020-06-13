b183673e22fc70e11257e6b21c78bb7864a0cb40
sid3github <siddharthpadwal3@gmail.com>
2020-06-13 16:11:24 +0530

    js practice

---
+++ home.js
@@ -0,0 +1,79 @@
+// var fruit = ['banana', 'mango', 'apple', 'lemon', 'orange'];
+
+// console.log(fruit.splice(2, 2));
+
+// var fruit = 'banana,apple,mango,carrot';
+
+// console.log(fruit.split(","));
+
+// let fruit = ['banana', 'mango', 'apple', 'lemon', 'orange'];
+
+// fruit = new Array('banana', 'mango', 'apple', 'lemon', 'orange');
+
+// fruit[3] = 'waterlemon';
+
+// for (let i in fruit) {
+//     console.log(fruit[i]);
+// }
+
+// console.log(fruit.sort());
+
+// let oldString = [];
+
+// oldString.push('Siddharth', 'Archisha', 'Gaurav');
+
+// console.log(oldString);
+
+// let newString = [];
+// newString.push('Pranit', 'Deexit', 'Ashwin');
+
+// let output = oldString.concat(newString);
+
+// console.log(output);
+
+// let developer = {
+//     firstname: 'Siddharth',
+//     lastname: 'Padwal',
+//     age: 27,
+//     height: 5.6,
+//     skill: [{
+//             frontend: 'javaScript',
+//         },
+//         {
+//             backend: 'python',
+//         }
+//     ],
+//     developerInfo: function () {
+//         this.firstname = 'Archisha';
+//         return this.firstname + ' ' + this.lastname;
+//     }
+// }
+
+// console.log(developer);
+// console.log(developer.skill[0]);
+// developer.age++;
+// console.log(developer.age);
+
+// console.log(developer.developerInfo());
+
+
+
+// switch (4) {
+//     case 1:
+//         workout = 'leg day';
+//         break;
+//     case 2:
+//         workout = 'arm day';
+//         break;
+//     case 3:
+//         workout = 'back day';
+//         break;
+//     case 4:
+//         workout = 'abs day';
+//         break;
+
+//     default:
+//         workout = 'rest day';
+// }
+
+// console.log(workout);
